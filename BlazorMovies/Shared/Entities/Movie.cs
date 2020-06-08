e3bf9e5e276b2220871b9c316f8738df4c6dfe08
aleph0mc <aleph0mc@gmail.com>
2020-06-08 22:39:25 +0200

    Commit demo page

---
+++ BlazorMovies/Shared/Entities/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMovies.Shared.Entities
+{
+    /// <summary>
+    /// The code in BlazorMovies.Shared project can be used in the Client or Server projects
+    /// </summary>
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
