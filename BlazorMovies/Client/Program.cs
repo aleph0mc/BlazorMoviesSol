23db37904dc4bf3800edcbbedd760c3a2ce8836a
aleph0mc <aleph0mc@gmail.com>
2020-06-10 22:09:01 +0200

    Working with components

--- BlazorMovies/Client/Program.cs
+++ BlazorMovies/Client/Program.cs
@@ -7,19 +7,36 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Net.NetworkInformation;
+using BlazorMovies.Client.Helpers;
 
 namespace BlazorMovies.Client
 {
     public class Program
     {
+        private static string _baseAddress;
+
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            // To configure all the servises for DI
+            _baseAddress = builder.HostEnvironment.BaseAddress;
+            ConfigureServices(builder.Services);
 
             await builder.Build().RunAsync();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions(); // used for auth. system
+            services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(_baseAddress) });
+            services.AddSingleton<SingletonService>();
+            services.AddTransient<TransientService>();
+            services.AddTransient<IRepository, RepositoryInMemory>();
         }
     }
 }
