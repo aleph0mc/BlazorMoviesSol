f6ad25da5a75d1eee49dc0604a3fdda48e655fcb
aleph0mc <aleph0mc@gmail.com>
2020-06-10 22:09:01 +0200

    Working with components

---
+++ BlazorMovies/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
+        {
+            //WRITE A CONSOLE LOG
+            await js.InvokeVoidAsync("console.log", "confirm message:", message);
+
+            return await js.InvokeAsync<bool>("confirm", message);
+        }
+
+        /// <summary>
+        /// Custom js function
+        /// The js file is in wwwroot\js\utilities.js
+        /// It is added to index.html file
+        /// </summary>
+        /// <param name="js"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static async ValueTask MyJsFunction(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("my_function", message);
+        }
+    }
+}
---
+++ BlazorMovies/Client/Helpers/RepositoryInMemory.cs
@@ -0,0 +1,21 @@
+﻿using BlazorMovies.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public class RepositoryInMemory : IRepository
+    {
+        public List<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+            new Movie {Title = "Spider-Man: Far From Home", ReleaseDate = new DateTime(2019, 7, 2)  },
+            new Movie {Title = "Hulk", ReleaseDate = new DateTime(2003, 11, 23)  },
+            new Movie {Title = "Terminator", ReleaseDate = new DateTime(1990, 7, 16)  }
+            };
+        }
+    }
+}
