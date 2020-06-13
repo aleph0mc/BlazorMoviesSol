a7b5f91051f46c123f96a406c0e25cdf162e3de3
aleph0mc <aleph0mc@gmail.com>
2020-06-13 13:21:39 +0200

    Movie components

--- BlazorMovies/Shared/Entities/Movie.cs
+++ BlazorMovies/Shared/Entities/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BlazorMovies.Shared.Entities
@@ -9,11 +10,16 @@ namespace BlazorMovies.Shared.Entities
     /// </summary>
     public class Movie
     {
-        public int Id { get; set; } = 1; //Temporary value
+        public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
-        public DateTime ReleaseDate { get; set; }
+        public string Summary { get; set; }
+        public bool InTheaters { get; set; }
+        public string Trailer { get; set; }
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
         public string Poster { get; set; }
-
+        public List<MoviesGenres> MoviesGenres { get; set; } = new List<MoviesGenres>();
         public string TitleBrief
         {
             get
